b511298a1ddab5592b8d02690097b62ddfcaf077
Thematveev <33023031+Thematveev@users.noreply.github.com>
2022-05-23 19:19:14 +0300

    Initial commit

---
+++ js/controller.js
@@ -0,0 +1,10 @@
+let main_block = document.querySelector(".main");
+let weather_block = document.querySelector(".weather");
+let explore_btn = document.querySelector(".main__button");
+
+function change_window() {
+    main_block.style.display = "none";
+    weather_block.style.display = "flex";
+}
+
+// explore_btn.addEventListener("click", change_window);
---
+++ js/main.js
@@ -0,0 +1,37 @@
+// temp city counrty wind comment
+
+let btn = document.querySelector(".main__button");
+let input = document.querySelector(".main__input");
+
+function exploreHandler() {
+    try {
+        city = input.value;
+        link = `http://api.weatherapi.com/v1/current.json?key=f22b48abdd4943b2af6165250211510&q=${city}&aqi=no`;
+
+        const request = new XMLHttpRequest();
+        request.open("GET", link, false);
+        request.send();
+        let data = JSON.parse(request.responseText);
+        console.log(data);
+
+        document.querySelector(".weather__country").innerHTML =
+            data.location.country;
+
+        document.querySelector(".weather__city").innerHTML = data.location.name;
+
+        let temp = Math.round(data.current.temp_c);
+        document.querySelector(".weather__temp").innerHTML = temp;
+
+        document.querySelector(".weather__comment").innerHTML =
+            data.current.condition.text;
+
+        document.querySelector(".wind__speed").innerHTML =
+            data.current.wind_kph;
+
+        change_window();
+    } catch {
+        alert("Incorrect city name!");
+    }
+}
+
+btn.addEventListener("click", exploreHandler);
